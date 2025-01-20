f52270baaefda66c6b8f52bea48cec0e3e2107a3
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-20 18:46:11 +0330

    add serializer to project

--- store/views.py
+++ store/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . import models
+from .serializers import ProductSerializer
+
 @api_view()
 def product_list(request):
-    return Response('hello')
+    queryset = models.Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def product_detail(request, pk):
+    product = get_object_or_404(models.Product, pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
