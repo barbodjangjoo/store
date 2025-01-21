3af3f42b5d4f379c203f34371c4bd43f86b52612
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-21 11:38:19 +0330

    add category to serializer and optimized queries

--- store/views.py
+++ store/views.py
@@ -7,13 +7,15 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    queryset = models.Product.objects.all()
+    queryset = models.Product.objects.select_related('category').all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data)
 
 @api_view()
 def product_detail(request, pk):
-    product = get_object_or_404(models.Product, pk)
+    product = get_object_or_404(
+        models.Product.objects.select_related('category'),
+         pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
