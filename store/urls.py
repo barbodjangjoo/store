1af9205164e529fd0b81ebd808ba2e925ebe9a4a
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-01 16:58:09 +0330

    replace cbv for category list func

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/', views.category_list, name='category-list'),
+    path('category/', views.CategoryList.as_view(), name='category-list'),
     path('category/<int:pk>/', views.category_detail, name='category-detail'),
     path('product/', views.ProductList.as_view(), name='product-list'),
-    path('product/<int:pk>/', views.product_detail, name='product-detail'),
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
 ]
 
--- store/views.py
+++ store/views.py
@@ -7,25 +7,13 @@ from rest_framework import status
 from . import models
 from . import serializers
 
-class ProductList(APIView):
+class CategoryList(APIView):
     def get(self, request):
-        queryset = models.Product.objects.select_related('category').all()
-        serializer = serializers.ProductSerializer(queryset, many=True, context={'request':request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = serializers.ProductSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['GET', 'POST'])
-def category_list(request):
-    if request.method == 'GET':
         queryset = models.Category.objects.prefetch_related('products').all()
         serializer = serializers.CategorySerializers(queryset, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
+    
+    def post(self, request):
         serializer = serializers.CategorySerializers(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -48,40 +36,38 @@ def category_detail(request, pk):
         category.delete()
         return Response('category were delete', status=status.HTTP_200_OK)
 
-# @api_view(['GET', 'POST'])
-# def product_list(request):
-#     if request.method == 'GET': 
-#         queryset = models.Product.objects.select_related('category').all()
-#         serializer = serializers.ProductSerializer(queryset, many=True, context={'request':request})
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = serializers.ProductSerializer(data = request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+class ProductList(APIView):
+    def get(self, request):
+        queryset = models.Product.objects.select_related('category').all()
+        serializer = serializers.ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
-    product = get_object_or_404(
+    def post(self, request):
+        serializer = serializers.ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ProductDetail(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(
         models.Product.objects.select_related('category'),
         pk=pk)
-    
-    if request.method == 'GET':
         serializer = serializers.ProductSerializer(product, context={'request':request})
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    
+    def put(self, request, pk):
+        product = get_object_or_404(
+        models.Product.objects.select_related('category'),
+        pk=pk)
         serializer = serializers.ProductSerializer(product ,data=request.data, context={'request':request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, pk):
+        product = get_object_or_404(
+        models.Product.objects.select_related('category'),
+        pk=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
-
-
-
-        
-
-
