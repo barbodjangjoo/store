07d057ed5786c26960b236135eb066658bb4528f
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-02 20:27:40 +0330

    add routers for project

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet, basename='product')
+router.register('categories', views.CategoryViewSet, basename='category')
+
 urlpatterns = [
-    path('category/', views.CategoryList.as_view(), name='category-list'),
-    path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
-    path('product/', views.ProductList.as_view(), name='product-list'),
-    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('', include(router.urls)),
 ]
+
+
+# urlpatterns = [
+#     path('category/', views.CategoryList.as_view(), name='category-list'),
+#     path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
+#     path('product/', views.ProductList.as_view(), name='product-list'),
+#     path('product/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+# ]
 
