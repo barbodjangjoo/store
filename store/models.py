4f210768f1f8c5a321dd5cb2d9231a289304fe9f
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-05 18:04:57 +0330

    update cart model with uuid

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Category(models.Model):
@@ -120,6 +121,7 @@ class Comment(models.Model):
     approved = ApprovedCommentManager()
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
