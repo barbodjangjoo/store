5bb6df5c35422d9b84272f9be453b3edbefe5dd8
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-14 15:45:14 +0330

    add jwt token

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # drf setting
 REST_FRAMEWORK ={
 'COERCE_DECIMAL_TO_STRING':False,
+'DEFAULT_AUTHENTICATION_CLASSES':(
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+)
+}
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES':('JWT', ),
 }
 
 AUTH_USER_MODEL = 'core.CustomUser'
--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 # codingyar.com/store/
