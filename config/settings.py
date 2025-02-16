672d1718bff8df9a9e031f4b0f17ba63cb57b9b6
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-16 14:33:09 +0330

    add lifetime token

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,6 +146,14 @@ REST_FRAMEWORK ={
 }
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES':('JWT', ),
+    'ACCESS_TOKEN_LIFTIME': timedelta(days=1)
+}
+
+DJOSER = {
+    'SERIALIZERS':{
+        'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
+    }
 }
 
 AUTH_USER_MODEL = 'core.CustomUser'
--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,10 @@
 from djoser.serializers import UserCreateSerializer as DjoserUserCreateSerializer
+from djoser.serializers import UserSerializer as DjoserUserSerializer
 
-class UserCreationSerializer(DjoserUserCreateSerializer):
+class UserCreateSerializer(DjoserUserCreateSerializer):
     class Meta(DjoserUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+
+class UserSerilaizer(DjoserUserSerializer):
+    class Meta(DjoserUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
